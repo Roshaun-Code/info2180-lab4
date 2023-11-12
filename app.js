74a6b4b4d8a6bb05719c9a2afcb262fe91792097
Roshaun <roshaunmarshall09@gmail.com>
2023-11-12 17:51:59 -0500

    Finished question 3

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     let submit = document.querySelector("button")
+    let result = document.getElementById("result")
+    let input = document.querySelector("input")
 
     submit.addEventListener("click", () => {
-        fetch("http://localhost:8080/superheroes.php", { mode: 'no-cors' })
+        fetch(`http://localhost:8080/superheroes.php?input=${input.value}`, { mode: 'no-cors' })
             .then(response => response.text())
             .then(data => {
-                alert(data)
+                result.innerHTML = data
+                console.log(data)
             })
             .catch (error => alert(error))
     })
