b4dd15b90c9b686418f291b360f8def015f70eb0
Roshaun <roshaunmarshall09@gmail.com>
2023-11-12 16:57:17 -0500

    Did number 2

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     let submit = document.querySelector("button")
-    const httprequest =  XMLHttpRequest()
-    let url = "http://localhost:8080/superheroes.php"
 
     submit.addEventListener("click", () => {
-        fetch(url)
-            .then(response => {
-                if (!response.ok){
-                    throw new Error("Error")
-                }
-                return response.text()
+        fetch("http://localhost:8080/superheroes.php", { mode: 'no-cors' })
+            .then(response => response.text())
+            .then(data => {
+                alert(data)
             })
-            .then (data => console.log(data))
-            .then (error => console.error("Error: ", error))
+            .catch (error => alert(error))
     })
 })
--- superheroes.php
+++ superheroes.php
@@ -62,7 +62,6 @@ $superheroes = [
       "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
   ], 
 ];
-
 ?>
 
 <ul>
